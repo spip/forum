e6880676648217ccf2f7db52ce6667170a813677
nicolas.dorigny@gmail.com <>
2016-04-15 15:58:49 +0000

    report de r96638 Générer des urls publiques dans les notifications de forums publics.

--- inc/email_notification_forum.php
+++ inc/email_notification_forum.php
@@ -42,7 +42,7 @@ function inc_email_notification_forum_dist ($t, $email, $contexte=array()) {
 		}
 		else if ($t['statut'] == 'publie') # forum publie
 		{
-			$url = generer_url_entite($id_forum, 'forum');
+			$url = generer_url_entite($id_forum, 'forum', '', 'forum'.$id_forum, false);
 		}
 		else #  forum modere, spam, poubelle direct ....
 		{
